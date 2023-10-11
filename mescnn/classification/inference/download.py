fa113039e1d2cdda90341caab1eff89751d9e2eb
Nicolik <nicola.altini@gmail.com>
2023-10-11 17:44:22 +0200

    classification download

--- mescnn/classification/inference/download.py
+++ mescnn/classification/inference/download.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 def download_classifier(net_name, target, train_version):
     return hf_hub_download(
         repo_id="MESCnn/MESCnn",
-        filename=f"mescnn/classification/logs/cnn/holdout/{net_name}_{target}_{train_version}.pth",
+        filename=f"classification/logs/cnn/holdout/{net_name}_{target}_{train_version}.pth",
         token="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",
         local_dir='./mescnn',
         local_dir_use_symlinks=False,
